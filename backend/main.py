12912a7a621cc5592f0657d5f228ecb5d47763f0
Kamil Warchol <kwarchol@akamai.com>
2020-12-12 01:00:50 +0100

    Allow running uvicorn through code

--- backend/main.py
+++ backend/main.py
@@ -1,3 +1,5 @@
+from contextlib import suppress
+
 from fastapi import FastAPI
 
 from backend.api import api
@@ -11,3 +13,9 @@ app.include_router(api, prefix=config.api_url_prefix)
 @app.get('/')
 async def get_root():
     return {'hello': 'world'}
+
+
+if __name__ == '__main__':
+    with suppress(ImportError):
+        import uvicorn
+        uvicorn.run('backend.main:app', reload=True)
