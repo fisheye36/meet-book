763808c8e7528067c05efa9ee6040dd30a94d007
Kamil Warchol <kwarchol@akamai.com>
2020-12-16 20:37:29 +0100

    Reword debug message

--- backend/database.py
+++ backend/database.py
@@ -24,7 +24,7 @@ class Database:
     def __del__(self) -> None:
         if hasattr(self, '_driver'):
             self._driver.close()
-            print('Ending DB session')
+            print('Ending DB connection')
 
 
 database = Database()
