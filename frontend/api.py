053c6954493556a72ff8c98fdcb7edee880e4e02
Kamil Warchol <kwarchol@akamai.com>
2020-12-18 22:57:13 +0100

    Add register page

--- frontend/api.py
+++ frontend/api.py
@@ -12,3 +12,12 @@ frontend_api = APIRouter()
 def login_page() -> str:
     template = jinja.get_template('login.html')
     return template.render(login_endpoint=config.api_url_prefix + api.url_path_for('login'))
+
+
+@frontend_api.get('/register', response_class=HTMLResponse)
+def register_page() -> str:
+    template = jinja.get_template('register.html')
+    return template.render(
+        register_endpoint=config.api_url_prefix + api.url_path_for('create_user'),
+        login_endpoint=frontend_api.url_path_for('login_page'),
+    )
